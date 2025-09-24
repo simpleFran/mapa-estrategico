fe09fb6b7237ea89a888a2afdfd4c64595189a83
simpleFran <frank.m.camargo@gmail.com>
2025-09-24 16:02:55 -0300

    chore: clean repo, update .gitignore and remove dist/node_modules/env from version control

--- backend/dist/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const env_1 = require("../env");
-const client_1 = require("@prisma/client");
-exports.prisma = new client_1.PrismaClient({
-    log: env_1.env.NODE_ENV === "dev" ? ["query"] : [],
-});
-//# sourceMappingURL=prisma.js.map
--- backend/dist/utils/geocalculo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.distanciaParaSegmento = distanciaParaSegmento;
-const geolib_1 = require("geolib");
-function distanciaParaSegmento(ponto, origem, destino) {
-    return (0, geolib_1.getDistanceFromLine)(ponto, origem, destino);
-}
-//# sourceMappingURL=geocalculo.js.map
